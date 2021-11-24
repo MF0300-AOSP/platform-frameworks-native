boundarydevices/frameworks_native

ENGR00215174 add GRALLOC_USAGE_HW_FBX usage for FramebufferSurface.
add GRALLOC_USAGE_HW_FBX usage for FramebufferSurface to indicate
second display.

Signed-off-by: Xiaowen Liu <b37945@freescale.com>

services/surfaceflinger/DisplayHardware/FramebufferSurface.cpp
@@ -59,9 +59,12 @@ FramebufferSurface::FramebufferSurface(HWComposer& hwc, int disp) :
 {
     mName = "FramebufferSurface";
     mBufferQueue->setConsumerName(mName);
-    mBufferQueue->setConsumerUsageBits(GRALLOC_USAGE_HW_FB |
-                                       GRALLOC_USAGE_HW_RENDER |
-                                       GRALLOC_USAGE_HW_COMPOSER);
+    int usage = GRALLOC_USAGE_HW_FB | GRALLOC_USAGE_HW_RENDER |
+                                       GRALLOC_USAGE_HW_COMPOSER;
+    if(disp != 0) {
+        usage |= GRALLOC_USAGE_HW_FBX;
+    }
+    mBufferQueue->setConsumerUsageBits(usage);
     mBufferQueue->setDefaultBufferFormat(mHwc.getFormat(disp));
     mBufferQueue->setDefaultBufferSize(mHwc.getWidth(disp),  mHwc.getHeight(disp));
     mBufferQueue->setSynchronousMode(true);

MF0300-AOSP/platform-frameworks-native

services/surfaceflinger/DisplayHardware/FramebufferSurface.cpp
@@ -61,9 +61,12 @@ FramebufferSurface::FramebufferSurface(HWComposer& hwc, int disp,
 {
     mName = "FramebufferSurface";
     mConsumer->setConsumerName(mName);
-    mConsumer->setConsumerUsageBits(GRALLOC_USAGE_HW_FB |
-                                       GRALLOC_USAGE_HW_RENDER |
-                                       GRALLOC_USAGE_HW_COMPOSER);
+    int usage = GRALLOC_USAGE_HW_FB | GRALLOC_USAGE_HW_RENDER |
+                                       GRALLOC_USAGE_HW_COMPOSER;
+    if(disp != 0) {
+        usage |= GRALLOC_USAGE_HW_FBX;
+    }
+    mConsumer->setConsumerUsageBits(usage);
     mConsumer->setDefaultBufferFormat(mHwc.getFormat(disp));
     mConsumer->setDefaultBufferSize(mHwc.getWidth(disp),  mHwc.getHeight(disp));
     mConsumer->setDefaultMaxBufferCount(NUM_FRAMEBUFFER_SURFACE_BUFFERS);